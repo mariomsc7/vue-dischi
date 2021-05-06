d9cf71f7504229a1f959cd1aa762c7b87804addd
mariomsc7 <mariomiscischia@gmail.com>
2021-05-06 16:05:02 +0200

    start + header

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="@/assets/logo.png" alt="">
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+    @import '@/scss/general.scss';
+
+    header {
+        height: 70px;
+        background: $header-bg-color;
+    }
+
+    .logo {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        padding: 10px;
+    }
+
+    .logo {
+        img {
+            width: 64px;
+            padding: 5px;
+        }
+    }
+
+</style>
